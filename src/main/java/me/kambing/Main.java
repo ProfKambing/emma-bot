121463a5740c041c28733339ec0901fe74cfe68e
ProfKambing <fawwaziqbal2008@gmail.com>
2021-08-16 09:06:11 +0800

    fixed gateway intent error

--- src/main/java/me/kambing/Main.java
+++ src/main/java/me/kambing/Main.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.exceptions.RateLimitedException;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -65,7 +66,7 @@ public class Main {
 
 
         JDABuilder.createDefault(token)
-
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .setActivity(Activity.playing("playing with kambing"))
                 .addEventListeners(new Raden(), new Troll(), new TrollToggler(), new FilterEvent(), new FilterToggler(), waiter, client.build())
