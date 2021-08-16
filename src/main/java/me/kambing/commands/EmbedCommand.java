cb5cb5aa1f496ec87f06f37aa437f12338599bda
ProfKambing <fawwaziqbal2008@gmail.com>
2021-08-16 12:10:58 +0800

    BRO

--- src/main/java/me/kambing/commands/EmbedCommand.java
+++ src/main/java/me/kambing/commands/EmbedCommand.java
@@ -22,7 +22,7 @@ public class EmbedCommand extends Command {
         try {
             var message = event.getMessage().getContentRaw().split(",");
 
-            event.getChannel().sendMessageEmbeds(new EmbedBuilder().setTitle(message[1]).setDescription(message[2]).setColor(message[3] == null ? event.getSelfMember().getColor() : new Color(Integer.parseInt(message[2]))).build()).queue();
+            event.getChannel().sendMessageEmbeds(new EmbedBuilder().setTitle(message[1]).setDescription(message[2]).setColor(message[3] == null ? event.getSelfMember().getColor() : new Color(Integer.parseInt(message[3]))).build()).queue();
 
         }catch (IndexOutOfBoundsException e) {
             event.getMessage().replyEmbeds(new EmbedBuilder().setTitle("Incorrect Format!").setDescription("Format:\n;embed ,<title>,<description>,<hexcolor>\nyou doesnt need to space between the ,").setColor(Color.CYAN).build()).queue();
